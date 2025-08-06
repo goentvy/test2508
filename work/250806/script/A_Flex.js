4fdf9812383d277e313640efe1536c8161410549
Entvy <entvyv@gmail.com>
2025-08-06 22:21:39 +0900

    test

--- work/250806/script/A_Flex.js
+++ work/250806/script/A_Flex.js
@@ -1,3 +1,4 @@
+// MenuBar Click Event
 $("#main-menu>li").mouseenter(function() {
     $(".sub_menu").stop().slideDown(500);
 });
@@ -6,39 +7,72 @@ $("#main-menu>li").mouseleave(function() {
     $(".sub_menu").stop().slideUp(500);
 });
 
-var slide = $("#slideImage>img");
-var slideCount = 0;
-var endCount = slide.length - 1;
+// image slide Event
+$(function(){
+  const slides = $("#slideImage > img");
+  let current = 0;
+  const total   = slides.length;
+  const height  = 300;
+  const duration = 800;
+  const delay    = 2500;
 
-var timer = setInterval("autoSlide()", 2500);
+  slides.css({ top: height + "px" })
+        .eq(0)
+        .css({ top: "0px" })
+        .addClass("active");
 
-function autoSlide() {
-    $(slide[slideCount]).stop().animate(
-        { top: "-300px" }, 1000, function () {
-            $(this).css({ top: "300px" });
-        }
-    );
-    slideCount++;
-    if (slideCount > endCount) {
-        slideCount = 0;
-    }
-    $(slide[slideCount]).stop().animate(
-        { top: "0" }, 1000);
-}
+  function autoSlide(){
 
-$(".notice_gallery_box>div").click(function (){
-    const notice = $(".notice_box")[0];
-    const gallery = $(".gallery_box")[0];
+    slides.eq(current)
+          .stop(true)
+          .animate({ top: -height + "px" }, duration);
+    
+    current = (current + 1) % total;
+    slides.eq(current)
+          .css({ top: height + "px" })
+          .stop(true)
+          .animate({ top: "0px" }, duration);
+  }
 
-    if(this === notice) {
-        $("#noticeContent").show();
-        $("#galleryContent").hide();
-        $("#noticeBox").css({ fontWeight: "700", backgroundColor: "black", color: "#fff" });
-        $("#galleryBox").css({ fontWeight: "400", backgroundColor: "#CCCACA", color: "#000" });
-    } else {
-        $("#noticeContent").hide();
-        $("#galleryContent").show();
-        $("#noticeBox").css({ fontWeight: "400", backgroundColor: "#CCCACA", color: "#000" });
-        $("#galleryBox").css({ fontWeight: "700", backgroundColor: "black", color: "#fff" });
-    }
+  const timer = setInterval(autoSlide, delay);
+});
+
+// Notice & Gallery Event
+$(function(){
+  const $noticeBtn  = $("#noticeBox");
+  const $galleryBtn = $("#galleryBox");
+  const $noticeCt   = $("#noticeContent");
+  const $galleryCt  = $("#galleryContent");
+
+  $noticeBtn.add($galleryBtn).click(function(){
+    $galleryCt.css({display: "flex"});
+    const isNotice = $(this).is($noticeBtn);
+
+    $noticeCt.toggle(isNotice);
+    $galleryCt.toggle(!isNotice);
+
+    $noticeBtn.toggleClass("active", isNotice);
+    $galleryBtn.toggleClass("active", !isNotice);
+  });
+});
+
+// PopUp Event
+$(function() {
+    $(".open-popup").click(function(e) {
+        e.preventDefault();
+        console.log();
+        let layer = $("<div></div>");
+        layer.addClass("popup-layer");
+        layer.html(`
+            <div class="popup">
+                <h2>${e.target.childNodes[0].data}</h2>
+                <p>여기에 내용이 들어갑니다.</p>
+                <button class="close">닫기</button>
+            </div>`);
+        $("body").append(layer);
+        
+        layer.find(".close").click(function() {
+            $(layer).remove();
+        })
+    })
 })
